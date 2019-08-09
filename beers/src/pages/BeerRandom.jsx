21005c05131bc8e6a1b1d2105dbfe413746311c6
Mark Trevail <trevail.m@gmail.com>
2019-08-09 17:06:38 +0200

    Random beer added

--- beers/src/pages/BeerRandom.jsx
+++ beers/src/pages/BeerRandom.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
 import Nav from '../components/Nav';
+import BeerTileDetail from '../components/BeerTileDetail';
+import axios from 'axios';
 
 class BeerRandom extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       beerData: []
+      }
+    }
+  
+    componentDidMount() {
+
+      axios.get(`https://ih-beers-api.herokuapp.com/beers/random`)
+      .then((beer) => {
+        this.setState({beerData: beer.data})
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    };
+
+  
   render() {
+
     return (
       <div>
         
         <Nav />
+        
+        <BeerTileDetail
+          {...this.state.beerData}
+        />
 
       </div>
     )
